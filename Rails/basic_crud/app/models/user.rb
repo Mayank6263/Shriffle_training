f9f4e668871c5f287e1e76570e9cd93390cbf695
Office's Ubuntu/linux <75mayanksolanki.com>
2025-09-23 18:34:46 +0530

    Lock version pushed

--- Rails/basic_crud/app/models/user.rb
+++ Rails/basic_crud/app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   # has_and_belongs_to_many :posts
   has_many :posts
-  
+  user = User.preload(:posts).where(id:8)#will work but
+  user = User.preload(:posts).where(posts: {id:8})#will work but
+
   # attr_accessor :ages
 	#in case of has_one to create an instance we uses create_tablenameInsingular instead of create
 	# has_one :product#, through: :profiles
--- Rails/basic_crud/db/schema.rb
+++ Rails/basic_crud/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_22_083831) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_23_080121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_22_083831) do
     t.jsonb "data_jsonb", default: {}
     t.bigint "user_id", null: false
     t.integer "status", default: 0
+    t.integer "lock_version"
     t.index ["data_jsonb"], name: "index_posts_on_data_jsonb", using: :gin
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
